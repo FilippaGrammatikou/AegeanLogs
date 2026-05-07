91278e9767e4f9956cf6b6c1f03f1f7c07254393
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-05-07 20:01:40 +0300

    feat: define initial Supplier entity with service category and relationships

--- src/AegeanLogs.Domain/Entities/Supplier.cs
+++ src/AegeanLogs.Domain/Entities/Supplier.cs
@@ -1,9 +1,18 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using AegeanLogs.Domain.Enums;
 
 namespace AegeanLogs.Domain.Entities;
 
-internal class Supplier
+public class Supplier
 {
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public ServiceCategory ServiceCategory { get; set; }
+    public string ContactEmail { get; set; } = string.Empty;
+    public string? PhoneNumber { get; set; }
+    public bool IsActive { get; set; }
+    public List<ServiceJobs> ServiceJobs { get; set; } = [];
+    public List<ApplicationUser> ApplicationUsers { get; set; } = [];
 }
