689eb8fbd0aec84b4721dff0f1503286113df641
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-05-07 20:02:40 +0300

    feat: define initial ClientCompany entity with vessel and user relationships

--- src/AegeanLogs.Domain/Entities/ClientCompany.cs
+++ src/AegeanLogs.Domain/Entities/ClientCompany.cs
@@ -1,9 +1,16 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace AegeanLogs.Domain.Entities;
 
-internal class ClientCompany
+public class ClientCompany
 {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string ContactEmail { get; set; }
+    public string? PhoneNumber { get; set; }
+    public bool IsActive { get; set; } = true;
+    public List<Vessel> Vessels { get; set; } = [];
+    public List<ApplicationUser> Users { get; set; } = [];
 }
