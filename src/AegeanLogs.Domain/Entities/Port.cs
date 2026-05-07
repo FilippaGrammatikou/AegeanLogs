d2ab96b1536be4b870f2185cb9f4634624c66267
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-05-07 20:02:08 +0300

    feat: define initial Port entity with location details and port calls

--- src/AegeanLogs.Domain/Entities/Port.cs
+++ src/AegeanLogs.Domain/Entities/Port.cs
@@ -1,9 +1,15 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace AegeanLogs.Domain.Entities;
 
-internal class Port
+public class Port
 {
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Country { set; get;  } = string.Empty;
+    public string UnLocode { get; set;  } = string.Empty;
+    public bool IsActive { get; set; } = true;
+    public List<PortCall> PortCalls { get; set; } = [];
 }
