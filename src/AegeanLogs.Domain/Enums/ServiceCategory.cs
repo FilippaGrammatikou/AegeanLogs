4719d175d30f662385e0f1b14c33aa4979cdb7bd
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-05-08 18:49:03 +0300

    feat: define ServiceCategory enum for operational service grouping

---
+++ src/AegeanLogs.Domain/Enums/ServiceCategory.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AegeanLogs.Domain.Enums;
+
+public enum ServiceCategory
+{
+    Nautical,
+    VesselSupply,
+    WasteAndEnvironmental,
+    Crew,
+    Technical,
+    Documentation,
+    CargoCoordination,
+    Security,
+    Medical,
+    Other  //Services that do not fit another defined category
+}
