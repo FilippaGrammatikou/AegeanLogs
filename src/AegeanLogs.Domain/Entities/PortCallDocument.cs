fe9a282a70d93e117f2e6d5049f662e8d1375c95
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-05-08 18:01:00 +0300

    feat: define initial PortCallDocument entity with upload and review tracking

--- src/AegeanLogs.Domain/Entities/PortCallDocument.cs
+++ src/AegeanLogs.Domain/Entities/PortCallDocument.cs
@@ -1,9 +1,25 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using AegeanLogs.Domain.Enums;
 
 namespace AegeanLogs.Domain.Entities;
 
-internal class PortCallDocument
+public class PortCallDocument
 {
+    public int Id { get; set; }
+    public int PortCallId { get; set; }
+    public PortCall PortCall { get; set; } = null!;
+    public string DocumentType { get; set; } = string.Empty;
+    public bool IsRequired { get; set; }
+    public string? FileName { get; set; }
+    public DocumentStatus Status { get; set; } = DocumentStatus.Required;
+    public DateTimeOffset? UploadedAt { get; set; }
+    
+    public int? UploadedByUserId { get; set; }
+    public ApplicationUser? UploadedByUser { get; set; }
+    public DateTimeOffset? CheckedAt { get; set; }
+    public  int? CheckedByUserId { get; set; }
+    public ApplicationUser? CheckedByUser { get; set; }
+    public string? RejectionReason { get; set; }
 }
