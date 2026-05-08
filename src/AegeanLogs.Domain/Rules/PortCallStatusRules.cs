5a974a21ff95a4f7b62a4b30af17781720783647
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-05-08 20:22:28 +0300

    feat: enforce valid PortCall status transitions

--- src/AegeanLogs.Domain/Rules/PortCallStatusRules.cs
+++ src/AegeanLogs.Domain/Rules/PortCallStatusRules.cs
@@ -1,9 +1,26 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using AegeanLogs.Domain.Enums;
 
 namespace AegeanLogs.Domain.Rules;
-
-internal class PortCallStatusRules
+public static class PortCallStatusRules
 {
+    public static bool CanMoveTo(
+        PortCallStatus currentStatus,
+        PortCallStatus nextStatus)
+    {
+        return currentStatus switch
+        {
+            PortCallStatus.Expected => nextStatus is PortCallStatus.Arrived or PortCallStatus.Cancelled,
+            PortCallStatus.Arrived => nextStatus is PortCallStatus.WorkInProgress or PortCallStatus.Cancelled,
+            PortCallStatus.WorkInProgress => nextStatus is PortCallStatus.ReadyToLeave or PortCallStatus.Cancelled,
+            PortCallStatus.ReadyToLeave => nextStatus is PortCallStatus.Departed,
+            PortCallStatus.Departed => nextStatus is PortCallStatus.Closed,
+
+            PortCallStatus.Closed => false,
+            PortCallStatus.Cancelled => false,
+            _ => false
+        };
+    }
 }
