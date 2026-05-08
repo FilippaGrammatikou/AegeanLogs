71d143205b2635b04ea484cf1f89deca1ac90ea6
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-05-08 17:59:04 +0300

    feat: define initial ServiceType entity with readiness and supplier requirements

--- src/AegeanLogs.Domain/Entities/ServiceType.cs
+++ src/AegeanLogs.Domain/Entities/ServiceType.cs
@@ -1,9 +1,21 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using AegeanLogs.Domain.Enums;
 
 namespace AegeanLogs.Domain.Entities;
 
-internal class ServiceType
+public class ServiceType
 {
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public ServiceCategory Category { get; set; }
+    public string? Description { get; set; }
+    public bool IsUsuallyRequired { get; set; }
+    public bool RequiresSupplier { get; set; }
+    public bool RequiredEvidence { get; set; }
+    public bool BlockReadinessWhenIncomplete { get; set; }
+    public int? DefaultDeadlineHoursNeforeEtd { get; set; }
+    public bool isActive { get; set; } = true;
+    public List<ServiceJob> ServiceJobs { get; set; } = [];
 }
