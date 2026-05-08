62897a25dff11d6c0ef7d381e6e87d710012592a
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-05-08 20:28:13 +0300

    feat: define Vessel entity for client-owned port-call vessels

--- src/AegeanLogs.Domain/Entities/Vessel.cs
+++ src/AegeanLogs.Domain/Entities/Vessel.cs
@@ -3,15 +3,14 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace AegeanLogs.Domain.Entities;
-
 public class Vessel
 {
     public int Id { get; set; }
     public int ClientCompanyId { get; set; }
-    public int ClientCompany ClientCompany{ get; set; } = null!;
+    public ClientCompany ClientCompany { get; set; } = null!;
     public string Name { get; set; } = string.Empty;
     public string ImoNumber { get; set; } = string.Empty;
-    public string VesselType { get; set; } string.Empty;
+    public string VesselType { get; set; } = string.Empty;
     public string Flag { get; set; } = string.Empty;
     public bool IsActive { get; set; } = true;
     public List<PortCall> PortCalls { get; set; } = [];
