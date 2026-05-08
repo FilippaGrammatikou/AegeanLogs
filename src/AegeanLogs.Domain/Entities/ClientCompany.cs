2b9f7e845a7be2d153f3a04a7c50335e3302ded1
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-05-08 20:27:14 +0300

    feat: add ClientCompany entity with contact and ownership details

--- src/AegeanLogs.Domain/Entities/ClientCompany.cs
+++ src/AegeanLogs.Domain/Entities/ClientCompany.cs
@@ -7,8 +7,8 @@ namespace AegeanLogs.Domain.Entities;
 public class ClientCompany
 {
     public int Id { get; set; }
-    public string Name { get; set; }
-    public string ContactEmail { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string ContactEmail { get; set; } = string.Empty;
     public string? PhoneNumber { get; set; }
     public bool IsActive { get; set; } = true;
     public List<Vessel> Vessels { get; set; } = [];
