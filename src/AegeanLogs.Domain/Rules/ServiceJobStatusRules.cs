3a875510d2a306a242c7af93f3b57b50ffab2667
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-05-08 20:23:05 +0300

    feat: define ServiceJobStatusRules for service workflow transitions

--- src/AegeanLogs.Domain/Rules/ServiceJobStatusRules.cs
+++ src/AegeanLogs.Domain/Rules/ServiceJobStatusRules.cs
@@ -1,9 +1,44 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using AegeanLogs.Domain.Enums;
 
 namespace AegeanLogs.Domain.Rules;
-
-internal class ServiceJobStatusRules
+public static class ServiceJobStatusRules
 {
+    public static bool CanMoveTo(
+        ServiceJobStatus currentStatus,
+        ServiceJobStatus nextStatus)
+    {
+        return currentStatus switch
+        {
+            ServiceJobStatus.Requested => nextStatus is ServiceJobStatus.Assigned
+                                                                or ServiceJobStatus.Blocked
+                                                                or ServiceJobStatus.Cancelled,
+
+            ServiceJobStatus.Assigned => nextStatus is ServiceJobStatus.InProgress or ServiceJobStatus.Blocked
+                                                              or ServiceJobStatus.Late
+                                                              or ServiceJobStatus.Cancelled,
+
+            ServiceJobStatus.InProgress => nextStatus is ServiceJobStatus.Done or ServiceJobStatus.Blocked
+                                                                or ServiceJobStatus.Late
+                                                                or ServiceJobStatus.Cancelled,
+
+            ServiceJobStatus.Done => nextStatus is ServiceJobStatus.Checked
+                                                       or ServiceJobStatus.Blocked,
+            
+            ServiceJobStatus.Checked =>false,
+
+            ServiceJobStatus.Blocked => nextStatus is ServiceJobStatus.Assigned
+                                                            or ServiceJobStatus.InProgress
+                                                            or ServiceJobStatus.Cancelled,
+
+            ServiceJobStatus.Late => nextStatus is ServiceJobStatus.InProgress
+                                                      or ServiceJobStatus.Done
+                                                      or ServiceJobStatus.Cancelled,
+
+            ServiceJobStatus.Cancelled => false,
+            _=> false
+        };
+    }
 }
