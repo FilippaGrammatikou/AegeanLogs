550bf6c05a42501e56482d72fcb6ad6a7a21a5e0
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-05-09 15:16:35 +0300

    feat: add document workflow transition rules

--- src/AegeanLogs.Domain/Rules/DocumentStatusRules.cs
+++ src/AegeanLogs.Domain/Rules/DocumentStatusRules.cs
@@ -1,9 +1,27 @@
-﻿using System;
+using AegeanLogs.Domain.Enums;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace AegeanLogs.Domain.Rules;
 
-internal class DocumentStatusRules
+public static class DocumentStatusRules
 {
+    public static bool CanMoveTo(
+        DocumentStatus currentStatus,
+        DocumentStatus nextStatus)
+    {
+        return currentStatus switch
+        {
+            DocumentStatus.Required => nextStatus is DocumentStatus.Uploaded or DocumentStatus.Missing,
+            DocumentStatus.Uploaded => nextStatus is DocumentStatus.Checked or DocumentStatus.Rejected
+                                                             or DocumentStatus.Missing,
+
+            DocumentStatus.Checked => false,
+            DocumentStatus.Missing => nextStatus is DocumentStatus.Uploaded,
+            DocumentStatus.Rejected => nextStatus is DocumentStatus.Uploaded or DocumentStatus.Missing,
+
+            _=> false
+        };
+    }
 }
