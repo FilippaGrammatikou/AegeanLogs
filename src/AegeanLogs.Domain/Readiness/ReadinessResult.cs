b7df04be02486f47f6ab242091f11939dd9864d5
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-05-09 17:31:09 +0300

    feat: model readiness evaluation result with blockers and risk level

--- src/AegeanLogs.Domain/Readiness/ReadinessResult.cs
+++ src/AegeanLogs.Domain/Readiness/ReadinessResult.cs
@@ -1,9 +1,14 @@
-﻿using System;
+using AegeanLogs.Domain.Enums;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace AegeanLogs.Domain.Readiness;
-
-internal class ReadinessResult
+public class ReadinessResult
 {
+    public int Score { get; set; }
+    public RiskLevel Risklevel { get; set; }
+    public bool CanMoveToReadyToLeave { get; set; }
+    public bool CanClose { get; set;  }
+    public List<ReadinessBlocker> Blocker { get; set; } = [];
 }
