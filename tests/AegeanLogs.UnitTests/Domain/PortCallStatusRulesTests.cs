c955fb0284fc262b512c6b516338428e22ba84a1
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-05-22 19:38:41 +0300

    test(domain): add port call status rule coverage

--- tests/AegeanLogs.UnitTests/Domain/PortCallStatusRulesTests.cs
+++ tests/AegeanLogs.UnitTests/Domain/PortCallStatusRulesTests.cs
@@ -1,10 +1,35 @@
+using AegeanLogs.Domain.Enums;
+using AegeanLogs.Domain.Rules;
+
 namespace AegeanLogs.UnitTests.Domain;
 
 public class PortCallStatusRulesTests
 {
     [Fact]
-    public void Test1()
+    public void CanMoveTo_WhenExpectedToArrived_ReturnsTrue()
     {
+        var result = PortCallStatusRules.CanMoveTo(PortCallStatus.Expected, PortCallStatus.Arrived);
+        Assert.True(result);
+    }
 
+    [Fact]
+    public void CanMoveTo_WhenExpectedToClose_ReturnsFalse()
+    {
+        var result = PortCallStatusRules.CanMoveTo(PortCallStatus.Expected, PortCallStatus.Closed);
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void CanMoveTo_WhenWorkInProgressToReady_ReturnsTrue()
+    {
+        var result = PortCallStatusRules.CanMoveTo(PortCallStatus.WorkInProgress, PortCallStatus.ReadyToLeave);
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void CanMoveTo_WhenClosedToArrived_ReturnsFalse()
+    {
+        var result = PortCallStatusRules.CanMoveTo(PortCallStatus.Closed, PortCallStatus.Arrived);
+        Assert.False(result);
     }
 }
