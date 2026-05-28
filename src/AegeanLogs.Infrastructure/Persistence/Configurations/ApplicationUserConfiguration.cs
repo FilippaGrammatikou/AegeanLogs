ca556cf8c0df2dda7ed2a9f5b827d76ee1a3aa4c
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-05-28 20:36:59 +0300

    feat:add EF Core configuration for application users

---
+++ src/AegeanLogs.Infrastructure/Persistence/Configurations/ApplicationUserConfiguration.cs
@@ -0,0 +1,32 @@
+using AegeanLogs.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AegeanLogs.Infrastructure.Persistence.Configurations;
+public class ApplictionUserConfiguration : IEntityTypeConfiguration<ApplicationUser>
+{
+    public void Configure(EntityTypeBuilder<ApplicationUser> builder)
+    {
+        builder.ToTable("ApplicationUsers");
+
+        builder.HasKey(user => user.Id);
+
+        builder.Property(user => user.Email).IsRequired().HasMaxLength(256);
+        builder.Property(user => user.DisplayName).IsRequired().HasMaxLength(100);
+        builder.Property(user => user.PasswordHash).IsRequired().HasMaxLength(500);
+        builder.Property(user => user.Role).IsRequired().HasMaxLength(40);
+        builder.Property(user => user.CreatedAt).IsRequired();
+
+        builder.HasIndex(user => user.Role);
+        builder.HasIndex(user => user.SupplierId);
+        builder.HasIndex(user => user.ClientCompanyId);
+
+        builder.HasOne(user => user.Supplier).WithMany(supplier => supplier.Users)
+                                                                       .HasForeignKey(user => user.SupplierId).OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasOne(user => user.ClientCompany).WithMany(ClientCompany => ClientCompany.Users)
+                                                                                  .HasForeignKey(user => user.ClientCompany)
+                                                                                  .OnDelete(DeleteBehavior.Restrict);
+
+    }
+}
