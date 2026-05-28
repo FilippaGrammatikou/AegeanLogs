4f7dc7e73c559496aa0a3971d9c046d3e47db34c
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-05-28 20:18:20 +0300

    refactor: move AegeanLogsDbContext to Infrastructure persistence layer

--- src/AegeanLogs.Infrastructure/Persistence/AegeanLogsDbContext.cs
+++ src/AegeanLogs.Infrastructure/Persistence/AegeanLogsDbContext.cs
@@ -1,9 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using AegeanLogs.Application.Common.Persistence;
+using AegeanLogs.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
-namespace AegeanLogs.Infrastructure.Persistence;
-
-internal class AegeanLogsDbContext
+namespace AegeanLogs.Application.Common.Persistence;
+public class AegeanLogsDbContext : DbContext, IAegeanLogsDbContext
 {
+    public AegeanLogsDbContext(DbContextOptions<AegeanLogsDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<ApplicationUser> ApplicationUsers => Set<ApplicationUser>();
+    public DbSet<ClientCompany> ClientCompanies => Set<ClientCompany>();
+    public DbSet<Vessel> Vessels => Set<Vessel>();
+    public DbSet<Port> Ports => Set<Port>();
+    public DbSet<PortCall> PortCalls => Set<PortCall>();
+    public DbSet<ServiceType> ServiceTypes => Set<ServiceType>();
+    public DbSet<ServiceRequirementRule> ServiceRequirementRules => Set<ServiceRequirementRule>();
+    public DbSet<Supplier> Suppliers => Set<Supplier>();
+    public DbSet<ServiceJob> ServiceJobs => Set<ServiceJob>();
+    public DbSet<PortCallDocument> PortCallDocuments => Set<PortCallDocument>();
+    public DbSet<AuditLogEntry> AuditLogEntries => Set<AuditLogEntry>();
+
+    //EF core will automatically discover whatever new entity config files created
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AegeanLogsDbContext).Assembly);
+        base.OnModelCreating(modelBuilder);
+    }
 }
