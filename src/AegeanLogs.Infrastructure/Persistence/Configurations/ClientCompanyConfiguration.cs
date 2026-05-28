c3c7e718090b2786350229d4373fd401d8a65c60
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-05-28 20:46:43 +0300

    feat: add ClientCompany entity configuration

---
+++ src/AegeanLogs.Infrastructure/Persistence/Configurations/ClientCompanyConfiguration.cs
@@ -0,0 +1,22 @@
+using AegeanLogs.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AegeanLogs.Infrastructure.Persistence.Configurations;
+
+public class ClientCompanyConfiguration : IEntityTypeConfiguration<ClientCompany>
+{
+    public void Configure(EntityTypeBuilder<ClientCompany> builder)
+    {
+        builder.ToTable("ClientCompanies");
+
+        builder.HasKey(clientCompany => clientCompany.Id);
+
+        builder.Property(clientCompany => clientCompany.Name).IsRequired().HasMaxLength(150);
+        builder.Property(clientCompany => clientCompany.ContactEmail).IsRequired().HasMaxLength(250);
+        builder.Property(clientCompany => clientCompany.PhoneNumber).IsRequired().HasMaxLength(20);
+        
+        builder.HasIndex(clientCompany => clientCompany.Name).IsUnique();
+        builder.HasIndex(clientCompany => clientCompany.IsActive);
+    }
+}
