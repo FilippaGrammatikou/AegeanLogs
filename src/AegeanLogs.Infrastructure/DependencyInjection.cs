72508810b8eeadc07a9e5983eb13259a1d94ba65
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-06-07 18:41:42 +0300

    feat(infrastructure): configure database context dependency injection

--- src/AegeanLogs.Infrastructure/DependencyInjection.cs
+++ src/AegeanLogs.Infrastructure/DependencyInjection.cs
@@ -1,6 +1,24 @@
+using AegeanLogs.Application.Common.Persistence;
+using AegeanLogs.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
 namespace AegeanLogs.Infrastructure;
-
-public class DependencyInjection
+public static class DependencyInjection
 {
+    public static IServiceCollection AddInfrastruction(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+        if(string.IsNullOrWhiteSpace(connectionString))
+        {
+            throw new InvalidOperationException("Connection string 'DefaultConnection' was not found.");
+        }
 
+        services.AddDbContext<AegeanLogsDbContext>(options =>options.UseSqlServer(connectionString));
+
+        services.AddScoped<IAegeanLogsDbContext>(provider => provider.GetRequiredService<AegeanLogsDbContext>());
+
+        return services;
+    }
 }
