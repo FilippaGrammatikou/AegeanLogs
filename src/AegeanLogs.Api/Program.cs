33775ba1b92ba6164e7083724a52b655b193756c
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-06-08 18:20:52 +0300

    chore(api): enable development data seeding in Program.cs

--- src/AegeanLogs.Api/Program.cs
+++ src/AegeanLogs.Api/Program.cs
@@ -1,15 +1,22 @@
 using AegeanLogs.Infrastructure;
+using AegeanLogs.Infrastructure.Persistence;
+using AegeanLogs.Infrastructure.Persistence.Seed;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-
 builder.Services.AddOpenApi();
 builder.Services.AddInfrastructure(builder.Configuration);
-
 var app = builder.Build();
+
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+
+    using var scope = app.Services.CreateScope();
+    var dbContext = scope.ServiceProvider.GetRequiredService<AegeanLogsDbContext>();
+    await dbContext.Database.MigrateAsync();
+    await DatabaseSeeder.SeedAsync(dbContext);
 }
 
 app.UseHttpsRedirection();
-app.Run();
+app.Run(); app.Run();
