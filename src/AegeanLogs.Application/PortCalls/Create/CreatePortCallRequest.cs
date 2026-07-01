57771a8513f9615236e6f18c6110fd23f0ed5a14
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-07-01 19:26:25 +0300

    feat(app):add create port call response model

--- src/AegeanLogs.Application/PortCalls/Create/CreatePortCallRequest.cs
+++ src/AegeanLogs.Application/PortCalls/Create/CreatePortCallRequest.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace AegeanLogs.Application.PortCalls;
+namespace AegeanLogs.Application.PortCalls.Create;
 public sealed class CreatePortCallRequest
 {
     public int VessselId {  get; set; }
---
+++ src/AegeanLogs.Application/PortCalls/Create/CreatePortCallResponse.cs
@@ -0,0 +1,19 @@
+using AegeanLogs.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AegeanLogs.Application.PortCalls.Create;
+internal class CreatePortCallResponse
+{
+    public int Id {  get; set; }
+    public int VesselId { get; set; }
+    public int PortId { get; set; }
+    public PortCallPurpose Purpose { get; set; }
+    public DateTimeOffset Eta { get; set; }
+    public DateTimeOffset Etd { get; set; }
+    public int AssignedAgentUserId { get; set; }
+    public PortCallStatus Status { get; set; }
+    public string? Notes { get; set; }
+    public DateTimeOffset CreatedAt { get; set; }
+}
