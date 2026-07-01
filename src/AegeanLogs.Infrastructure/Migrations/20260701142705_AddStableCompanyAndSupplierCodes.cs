88f197bf5708f07a07ae24f8b2d86dbfe14171a6
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-07-01 17:43:14 +0300

    feat(persistence):add stable company and supplier codes

--- src/AegeanLogs.Infrastructure/Migrations/20260701142705_AddStableCompanyAndSupplierCodes.cs
+++ src/AegeanLogs.Infrastructure/Migrations/20260701142705_AddStableCompanyAndSupplierCodes.cs
@@ -1,7 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
-
 namespace AegeanLogs.Infrastructure.Migrations
 {
     /// <inheritdoc />
@@ -15,16 +14,80 @@ namespace AegeanLogs.Infrastructure.Migrations
                 table: "Suppliers",
                 type: "nvarchar(100)",
                 maxLength: 100,
-                nullable: false,
-                defaultValue: "");
+                nullable: true);
 
             migrationBuilder.AddColumn<string>(
                 name: "Code",
                 table: "ClientCompanies",
                 type: "nvarchar(100)",
                 maxLength: 100,
+                nullable: true);
+
+            migrationBuilder.Sql(
+                                        """
+                                        UPDATE [ClientCompanies]
+                                        SET [Code] =
+                                            CASE
+                                                WHEN [ContactEmail] = N'ops@aegeanblue.example'
+                                                     OR [Name] = N'Aegean Blue Shipping Ltd'
+                                                    THEN N'AEGEAN_BLUE'
+
+                                                WHEN [ContactEmail] = N'operations@hellenicbulk.example'
+                                                     OR [Name] = N'Hellenic Bulk Operators'
+                                                    THEN N'HELLENIC_BULK'
+
+                                                ELSE CONCAT(N'CLIENT_', [Id])
+                                            END
+                                        WHERE [Code] IS NULL;
+                                        """);
+
+            migrationBuilder.Sql(
+                                        """
+                                        UPDATE [Suppliers]
+                                        SET [Code] =
+                                            CASE
+                                                WHEN [ContactEmail] = N'dispatch@piraeusmarinesupplies.example'
+                                                     OR [Name] = N'Piraeus Marine Supplies'
+                                                    THEN N'PIRAEUS_MARINE_SUPPLIES'
+
+                                                WHEN [ContactEmail] = N'ops@atticawaste.example'
+                                                     OR [Name] = N'Attica Waste Services'
+                                                    THEN N'ATTICA_WASTE_SERVICES'
+
+                                                WHEN [ContactEmail] = N'booking@aegeancrewtransport.example'
+                                                     OR [Name] = N'Aegean Crew Transport'
+                                                    THEN N'AEGEAN_CREW_TRANSPORT'
+
+                                                WHEN [ContactEmail] = N'service@hellastechmarine.example'
+                                                     OR [Name] = N'Hellas Technical Marine'
+                                                    THEN N'HELLAS_TECHNICAL_MARINE'
+
+                                                ELSE CONCAT(N'SUPPLIER_', [Id])
+                                            END
+                                        WHERE [Code] IS NULL;
+                                        """);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Code",
+                table: "Suppliers",
+                type: "nvarchar(100)",
+                maxLength: 100,
                 nullable: false,
-                defaultValue: "");
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Code",
+                table: "ClientCompanies",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100,
+                oldNullable: true);
 
             migrationBuilder.CreateIndex(
                 name: "IX_Suppliers_Code",
