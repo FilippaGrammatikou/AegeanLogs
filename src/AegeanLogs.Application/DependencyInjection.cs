3f923353cb34b78bce33f7100c0cee4e62635289
Filippa Grammatikou <filippagrammatikou@gmail.com>
2026-07-02 18:24:28 +0300

    feat(application):register port call creation dependencies

---
+++ src/AegeanLogs.Application/DependencyInjection.cs
@@ -0,0 +1,14 @@
+using AegeanLogs.Application.PortCalls.Create;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using System.Runtime.CompilerServices;
+
+namespace AegeanLogs.Application;
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddScoped<IValidator<CreatePortCallRequest>, CreatePortCallValidator>();
+        return services;
+    }
+}
